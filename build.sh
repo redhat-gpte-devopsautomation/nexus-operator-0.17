f7b98e9ecb690d3eedaa862a43740f1745137ec9
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-11-28 15:59:54 -0500

    Updated with Readme and new instructions

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Update latest Roles
+rm -rf roles
+mkdir roles
+git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
+cp -R ansible-operator-roles/roles/nexus-ocp ./roles
+cp ansible-operator-roles/playbooks/nexus.yaml ./playbook.yaml
+rm -rf ansible-operator-roles
+
+# Now build the Operator
 operator-sdk build quay.io/wkulhanek/nexus-operator:v0.0.1
 docker push quay.io/wkulhanek/nexus-operator:v0.0.1
