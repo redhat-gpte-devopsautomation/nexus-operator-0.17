288722980381aef6918b882c28698275788ed79c
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-12-13 17:46:04 -0500

    Updated to 0.0.2

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ cp ansible-operator-roles/playbooks/nexus.yaml ./playbook.yaml
 rm -rf ansible-operator-roles
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/nexus-operator:v0.0.1
-docker push quay.io/wkulhanek/nexus-operator:v0.0.1
+operator-sdk build quay.io/wkulhanek/nexus-operator:v0.0.2
+docker push quay.io/wkulhanek/nexus-operator:v0.0.2
