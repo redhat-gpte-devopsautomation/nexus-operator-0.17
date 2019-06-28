8deb64c79fb47195e982b4c86a5ba7e9fd73d78c
Jim Rigsbee <jrigsbee@redhat.com>
2019-06-27 23:14:29 -0400

    Provide example for new repo construction vars in CR

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Update latest Roles
 rm -rf roles
 mkdir roles
-git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
+git clone https://github.com/jimrigsbee/ansible-operator-roles
 cp -R ansible-operator-roles/roles/nexus-ocp ./roles
 cp ansible-operator-roles/playbooks/nexus.yaml ./playbook.yml
 rm -rf ansible-operator-roles
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/nexus-operator:v0.8.1
-docker push quay.io/wkulhanek/nexus-operator:v0.8.1
+operator-sdk build quay.io/redhattraining/nexus-operator:v0.8.2
+docker push quay.io/redhattraining/nexus-operator:v0.8.2
