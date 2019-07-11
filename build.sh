9c190ebb3f3d071428f4121f15725434f94801f9
Jim Rigsbee <jrigsbee@redhat.com>
2019-07-11 13:41:07 -0400

    Fixed problems with updating CR status when run as operator 1) Added oc client to operator container 2) Added additional permissions needed to oc cp into nexus container

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ git clone https://github.com/jimrigsbee/ansible-operator-roles
 cp -R ansible-operator-roles/roles/nexus-ocp ./roles
 cp ansible-operator-roles/playbooks/nexus.yaml ./playbook.yml
 rm -rf ansible-operator-roles
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.4/openshift-client-linux-4.1.4.tar.gz
+mkdir client
+tar xzf openshift-client-linux-4.1.4.tar.gz -C client
+rm -f openshift-client-linux-4.1.4.tar.gz
 
 # Now build the Operator
-operator-sdk build quay.io/redhattraining/nexus-operator:v0.8.2
-docker push quay.io/redhattraining/nexus-operator:v0.8.2
+operator-sdk build quay.io/gpte-devops-automation/nexus-operator:v0.8.1
+docker push quay.io/gpte-devops-automation/nexus-operator:v0.8.1
+
+rm -fr client
