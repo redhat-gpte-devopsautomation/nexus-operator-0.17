857f0f898243352485eb5d685e26c29f0977d4db
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-07-19 14:31:57 -0400

    Updated for oc 4.1.6

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
 cp -R ansible-operator-roles/roles/nexus-ocp ./roles
 cp ansible-operator-roles/playbooks/nexus.yaml ./playbook.yml
 rm -rf ansible-operator-roles
-wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.4/openshift-client-linux-4.1.4.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.6/openshift-client-linux-4.1.6.tar.gz
 mkdir client
-tar xzf openshift-client-linux-4.1.4.tar.gz -C client
-rm -f openshift-client-linux-4.1.4.tar.gz
+tar xzf openshift-client-linux-4.1.6.tar.gz -C client
+rm -f openshift-client-linux-4.1.6.tar.gz
 
 # Now build the Operator
 operator-sdk build quay.io/gpte-devops-automation/nexus-operator:v0.8.1
